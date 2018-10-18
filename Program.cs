668707e1a230929656f7c8698f9e5d74f450c1ce
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-17 18:36:45 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Hey";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 5;
+            bool outOfGuesses = false;
+
+
+
+            while(guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter a guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses)
+            {
+                Console.Write("You Win! Jk, nope you lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+
+                      
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
